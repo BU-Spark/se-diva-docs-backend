4c214ef3f7d8ce791c75e2421b85731c830ac63a
vinaymetlapalli-github <vinaymet@bu.edu>
2022-11-14 13:25:05 -0500

    created function for read/write to mongo, added to API

--- application_form_api/main.py
+++ application_form_api/main.py
@@ -1,6 +1,9 @@
 from typing import List
 from fastapi import FastAPI
 from models import Applicant
+import pymongo
+from se-diva-docs-backend.application_form_api.mongodb_python_script.mongo_test import *
+
 
 app = FastAPI()
 
@@ -8,7 +11,8 @@ db: List[Applicant] = []
 
 @app.post("/applicants/add")
 def store_applicants(applicant: Applicant):
-    db.append(applicant)
+    # db.append(applicant)
+    write_to_mongo(applicant, 'ApplicationForm', 'SubmittedApplications')
     return applicant
 
 @app.get("/applicants/view")
