2bab22379594f7acc461df35ed3601ae04faca69
abhinoorbu <abhinoor@bu.edu>
2022-11-30 17:27:16 -0500

    completed resume upload download

--- application_form_api/main.py
+++ application_form_api/main.py
@@ -1,17 +1,19 @@
-from typing import List
 from fastapi import FastAPI, UploadFile, File
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from models import Applicant
 import mongo_test
-from os import getcwd
-from fastapi.responses import FileResponse
-import base64
-
+from fastapi.responses import Response
+from starlette_validation_uploadfile import ValidateUploadFileMiddleware
 
 app = FastAPI()
 router = APIRouter()
-db: List[Applicant] = []
+app.add_middleware(
+        ValidateUploadFileMiddleware,
+        app_path="/applicants/resume/upload",
+        max_size=31457280, #30 Megabytes max upload
+        file_type=["application/pdf"]
+)
 
 @app.post("/applicants/add")
 def store_applicants(applicant: Applicant):
@@ -25,14 +27,17 @@ def view_applicants():
 
 @app.post("/applicants/resume/upload")
 async def upload_file(upload_file: UploadFile = File(...)):
+    if upload_file.content_type != "application/pdf":
+        return JSONResponse(content={"error": "invalid file type. Only PDF accepted", "upload_status":"data upload not successful"}, status_code=422)
     with open(upload_file.filename, 'wb') as image:
         content = await upload_file.read()
         image.write(content)
         image.close()
         mongo_test.upload_file_to_mongo('ApplicationForm', 'SubmittedApplications', content, upload_file.filename)
-    return JSONResponse(content={"filename": upload_file.filename}, status_code=200)
+    return JSONResponse(content={"filename": upload_file.filename, "upload_status":"data uploaded success"}, status_code=200)
+        
 
 @app.get("/applicants/downloadresume/{name_file}")
 def download_file(name_file: str):
     fileFromDB = mongo_test.download_file_from_mongo('ApplicationForm', name_file)
-    return FileResponse(fileFromDB, media_type='application/octet-stream', filename=name_file)
+    return Response(fileFromDB, media_type='application/pdf')
