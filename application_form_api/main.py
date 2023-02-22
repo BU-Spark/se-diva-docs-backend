97d86eb70cd09ba958e5bcaa1d963582ccf9d50d
abhinoorbu <abhinoor@bu.edu>
2023-02-22 11:42:20 -0500

    added webhook

--- application_form_api/main.py
+++ application_form_api/main.py
@@ -5,6 +5,12 @@ from models import Applicant
 import mongo_test
 from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request, HTTPException
+import stripe
+
+# Set your Stripe API key and webhook signing secret
+stripe.api_key = "sk_test_51MbreiIOQGSqv0xRllrwIKir09GURs4U3QYiLXSyKTiWqBBAoyx21Jum6e20GJpVgTg2B8f8zPz0w2D4ewIdUAWf00EUNTiFyg"
+webhook_secret = "whsec_34d1b16bc211ba244123bf9fccebf7e286632563298ca36bc186df76d5bf09b6"
 
 app = FastAPI()
 router = APIRouter()
@@ -30,6 +36,7 @@ subscription_tier_list = {
     "doctor": 300
 }
 
+
 @app.post("/applicants/add")
 def store_applicants(applicant: Applicant):
     # db.append(applicant)
@@ -52,15 +59,42 @@ async def upload_file(upload_file: UploadFile = File(...)):
         image.close()
         mongo_test.upload_file_to_mongo('ApplicationForm', 'SubmittedApplications', content, upload_file.filename)
     return JSONResponse(content={"filename": upload_file.filename, "upload_status":"data uploaded success"}, status_code=200)
-        
+
 
 @app.get("/applicants/downloadresume/{name_file}")
 def download_file(name_file: str):
     fileFromDB = mongo_test.download_file_from_mongo('ApplicationForm', name_file)
     return Response(fileFromDB, media_type='application/pdf')
 
+
 @app.post("/applicants/requestpayment")
 def requestpayment(applicant: Applicant):
     applicant_dict = applicant.dict()
     mongo_test.create_payment(applicant_dict["primary_email"], subscription_tier_list[applicant_dict["applicant_status"]["subscription_tier"]])
-    return JSONResponse(content={"success": "Applicant Approved, Payment Requeste"}, status_code=200)
+    return JSONResponse(content={"success": "Applicant Approved, Payment Requested"}, status_code=200)
+
+
+@app.post('/webhook')
+async def handle_webhook(request: Request):
+    payload = await request.body()
+    sig_header = request.headers.get('Stripe-Signature')
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, webhook_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        raise HTTPException(status_code=400, detail=str(e))
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        raise HTTPException(status_code=401, detail=str(e))
+
+    # Handle the event
+    if event.type == 'payment_intent.succeeded':
+        payment_intent = event.data.object
+        return JSONResponse(content={'received': True})
+        # Do something with the payment_intent object, e.g. mark the order as paid
+
+    # Return a 200 response to acknowledge receipt of the event
+    return JSONResponse(content={'test': True})
