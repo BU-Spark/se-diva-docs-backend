6318e7961dd54ab96155bfe4bb82a3d2a0d76d48
abhinoorbu <abhinoor@bu.edu>
2023-03-29 18:40:44 -0400

    update

--- application_form_api/main.py
+++ application_form_api/main.py
@@ -122,7 +122,7 @@ def download_file(name_file: str):
 @app.post("/applicants/approveapplicant")
 def requestpayment(applicant: Applicant):
     applicant_dict = applicant.dict()
-    return mongo_test.send_payment(applicant_dict["id"])
+    return mongo_test.send_payment(applicant_dict["id"], applicant_dict["applicant_status"]["subscription_tier"])
 
 
 @app.post('/webhook')
