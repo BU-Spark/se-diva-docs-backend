a7f42ac009038efdb945dc2e65603bbe87ece8f9
vinaymet <vinaymet@bu.edu>
2023-03-29 19:44:33 -0400

    code cleanup

--- application_form_api/mongo_test.py
+++ application_form_api/mongo_test.py
@@ -138,7 +138,6 @@ def send_payment(u_id,subscription_tier):
         source_collection = db['SubmittedApplications']
         target_collection = db['ApprovedApplications']
     except Exception as e:
-        print('mongo connection error')
         return JSONResponse(content={'error':'mongo connection error'}, status_code=400)
 
     # Find the document with "id" field equals to U_ID
@@ -154,7 +153,6 @@ def send_payment(u_id,subscription_tier):
     # Move the Applicant to Approved Applications
 
     document = source_collection.find_one({"id": id})
-    print(document)
 
     target_collection.insert_one(document)
 
@@ -204,7 +202,6 @@ def send_payment(u_id,subscription_tier):
         metadata={"id": id}
     )
     else:
-        print('subscription tier not assigned')
         return JSONResponse(content={'error': 'Subscription Tier Not Assigned'}, status_code=400)
 
     # Send Email ----
@@ -235,12 +232,10 @@ def send_payment(u_id,subscription_tier):
         #server.sendmail(from_email, to_email, message.as_string())
     except Exception as e:
         # Return error message if email not sent successfully
-        print('email not sent')
         return {'error': 'email not sent'}
 
     # Close the server connection
     #server.quit()
-    print('email sent successfully')
     return JSONResponse(content={'success': 'Email sent'}, status_code=200)
 
 def get_all_approved():
