113c57dc97468cbcfdd7125b3b25481b401359f4
abhinoorbu <abhinoor@bu.edu>
2023-04-25 12:37:57 -0400

    cleared directory, and organized files

--- mongodb_python_script/shard_collection.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://vinaydivadocs:divadocs@divadocsmemberportal.zhjdqu2.mongodb.net/?retryWrites=true&w=majority")
-client.admin.command('enableSharding', 'ApplicationForm')
-#client.admin.command('enableSharding', 'SubmittedApplications')
